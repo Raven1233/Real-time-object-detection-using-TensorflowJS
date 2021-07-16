ad43d6bede9b46b3c906fb432fd87eb742345b3e
SubhadeepDas <subhadeepdasasd@gmail.com>
2021-07-16 13:59:43 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -50,7 +50,6 @@ class App extends React.Component {
   renderPredictions = predictions => {
     const ctx = this.canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // Font options.
     const font = "16px sans-serif";
     ctx.font = font;
     ctx.textBaseline = "top";
@@ -59,21 +58,18 @@ class App extends React.Component {
       const y = prediction.bbox[1];
       const width = prediction.bbox[2];
       const height = prediction.bbox[3];
-      // Draw the bounding box.
       ctx.strokeStyle = "#00FFFF";
       ctx.lineWidth = 4;
       ctx.strokeRect(x, y, width, height);
-      // Draw the label background.
       ctx.fillStyle = "#00FFFF";
       const textWidth = ctx.measureText(prediction.class).width;
-      const textHeight = parseInt(font, 10); // base 10
+      const textHeight = parseInt(font, 10); 
       ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
     });
 
     predictions.forEach(prediction => {
       const x = prediction.bbox[0];
       const y = prediction.bbox[1];
-      // Draw the text last to ensure it's on top.
       ctx.fillStyle = "#000000";
       ctx.fillText(prediction.class, x, y);
     });
